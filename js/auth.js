898476b762ead9f15b8c922b8610685a4307ae56
Collin Krum <collindk94@gmail.com>
2026-08-08 01:03:52 -0400

    Load league history archive tabs

--- js/auth.js
+++ js/auth.js
@@ -29,6 +29,7 @@
 
   function loadScript(src){return new Promise(resolve=>{const s=document.createElement('script');s.src=src;s.onload=resolve;s.onerror=resolve;document.head.appendChild(s)})}
   await loadScript('js/league-content.js');
+  await loadScript('js/history-layout.js');
   await loadScript('js/playoffs.js');
   await loadScript('js/admin.js');
   await loadScript('js/league-admin.js');
